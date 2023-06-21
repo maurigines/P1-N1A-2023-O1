7d7e04ed0c4e33afe82a56daeee03123117da6e1
maurigines <maurimar14@gmail.com>
2023-06-21 20:54:25 -0300

    Update funciones.js

--- js/funciones.js
+++ js/funciones.js
@@ -1,33 +1,24 @@
-// Luego de la carga del HTML busca los section para mostrar y ocultar
-document.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('load', function() {
     const enlacesNav = document.querySelectorAll('nav a');
-  
+   
    enlacesNav.forEach(function(enlace) {
       enlace.addEventListener('click', function(evento) {
         evento.preventDefault();
   
        const seccionId = this.getAttribute('href').substring(1);
   
-       const secciones = document.querySelectorAll('section');
-  
-        // Recorre las secciones y las muestra ocultando al resto
-        secciones.forEach(function(seccion) {
-          if (seccion.id === seccionId) {
-            seccion.style.display = 'block'; 
-          } else {
-            seccion.style.display = 'none'; 
-          }
-        });
+      mostrarSeccion(seccionId);
+       
       });
     });
   
-    // Muestra la sección 'Principal' al inicio y oculta el resto
-    const secciones = document.querySelectorAll('section');
-    secciones.forEach(function(seccion) {
-      if (seccion.id === 'principal') {
-        seccion.style.display = 'block'; 
-      } else {
-        seccion.style.display = 'none'; 
-      }
-    });
+   mostrarSeccion('principal'); 
   });
+  
+  function mostrarSeccion(section){
+    const secciones = document.querySelectorAll('section');
+   for(const seccion of secciones) seccion.style.display = 'none';
+   console.log(section);
+  document.getElementById(section).style.display = 'block';
+   
+  }
